b6c57ecc038a212836eec54e695da701eda3c833
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-07-28 21:38:28 +0700

    feat: integration get profile

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import { useEffect } from 'react';
+import Sidebar from '../../components/sidebar';
+import useAuth from '../../utils/hooks/useAuth';
+import axios from 'axios';
+
+const code = new URLSearchParams(window.location.search).get('code');
 
 const Home = () => {
-  return <div>Home</div>;
+  const { accessToken } = useAuth(code);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (!accessToken) return;
+        const res = await axios.get('https://api.spotify.com/v1/me', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+          }
+        });
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [accessToken]);
+
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
 };
 
 export default Home;
