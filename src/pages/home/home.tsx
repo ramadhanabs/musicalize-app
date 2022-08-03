ed292c7f4bcb56c9723af6136ccc72275a563e3d
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-08-03 09:09:20 +0700

    feat: integration get spotify categories

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,24 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Sidebar from '../../components/sidebar';
 import useAuth from '../../utils/hooks/useAuth';
+import AppLayout from '../../components/layout';
+import CardCategory from '../../components/cards/category';
 import axios from 'axios';
 
 const code = new URLSearchParams(window.location.search).get('code');
 
 const Home = () => {
   const { accessToken } = useAuth(code);
+  const [categories, setCategories] = useState<any[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         if (!accessToken) return;
-        const res = await axios.get('https://api.spotify.com/v1/me', {
+        const res = await axios.get('https://api.spotify.com/v1/browse/categories', {
           headers: {
             Authorization: `Bearer ${accessToken}`,
             'Content-Type': 'application/json'
           }
         });
         console.log(res);
+        setCategories(res.data.categories.items);
       } catch (error) {
         console.log(error);
       }
@@ -30,6 +34,9 @@ const Home = () => {
   return (
     <div>
       <Sidebar />
+      <AppLayout>
+        {categories.length > 0 && categories.map((item) => <CardCategory {...item} />)}
+      </AppLayout>
     </div>
   );
 };
